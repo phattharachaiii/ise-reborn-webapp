c7c9ad0e595a07b3b902f6e4f98bc63e80a09d2a
Phattharachai Leeyutthanon <phattharachai2547@gmail.com>
2025-09-28 20:07:22 +0700

    Initial Project Structure fontend & backend not create gitignore and choose backend yet

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="justify-center items-center flex flex-col h-screen">
+	<h1>Welcome to Our project hope u guys enjoy funny xdxd.</h1>
+	<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1"
+       class="mt-4 p-4 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition duration-300"
+		>Don't click me !!!</a
+	>
+</div>
